88c662d262820a9e56643b012588bd70ee0fe0fa
Kugler Tamas <tamas.kugler@gmail.com>
2015-10-19 16:28:36 +0200

    NEXTEUROPA-6160 modules moved to lib
    Former-commit-id: d67ee3865f0b840aff12ac14ba15856b6e6d7020

---
+++ views-view-nexteuropa-listing.tpl.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @file
+ * View template to display a news carousel.
+ *
+ * @ingroup views_templates
+ */
+?>
+<div class="listing__wrapper<?php print $listing_columns . $listing_wrapper_modifier; ?>">
+  <?php foreach ($columns as $column): ?>
+    <div class="listing<?php print isset($ds_view_mode) ? $ds_view_mode : '' . $listing_modifier; ?>">
+    <?php foreach ($column as $id => $row): ?>
+      <div class="listing__item">
+        <?php print $row; ?>
+      </div>
+    <?php endforeach; ?>
+    </div>
+  <?php endforeach; ?>
+</div>

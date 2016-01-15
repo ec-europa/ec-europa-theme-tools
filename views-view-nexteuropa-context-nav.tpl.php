57c18d5319e8f2f2f341e86894b77af4ccc9d37a
Kalin Chernev <kalin.chernev@gmail.com>
2016-01-15 13:54:34 +0100

    NEXTEUROPA-7864: Updates
    Former-commit-id: 6b1049450f9b3f344f6748dd7847dd4964025fa7
    Former-commit-id: afd5a51a63d741aa2520bd44415206b61509b85d

---
+++ views-view-nexteuropa-context-nav.tpl.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @file
+ * View template to contextual navigation.
+ *
+ * @ingroup views_templates
+ */
+?>
+<span class="context-nav__label">Part of collections:</span>
+<div class="context-nav__items">
+  <span class="context-nav__item">
+    <a href="http://ec.europa.eu/europeaid/about-development-and-cooperation-europeaid_en">International Cooperation and Development</a>
+  </span>
+</div>

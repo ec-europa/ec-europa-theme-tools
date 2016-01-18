6c52a5e044229cb885876924b65e30ba505d4041
Kalin Chernev <kalin.chernev@gmail.com>
2016-01-18 10:39:39 +0100

    NEXTEUROPA-7864: Group label will be changed dynamically, because it is hard to make it consistent otherwise
    Former-commit-id: b886f6368da5a99e6be4031e5ba97459a54749cb
    Former-commit-id: dc7233d1d41e6fe778785f5ce9f7699f7080876d

--- views-view-nexteuropa-context-nav.tpl.php
+++ views-view-nexteuropa-context-nav.tpl.php
@@ -7,7 +7,7 @@
  * @ingroup views_templates
  */
 ?>
-<span class="context-nav__label"><?php print $navigation_title; ?></span>
+<span class="context-nav__label"><?php print $navigation_label; ?></span>
 <div class="context-nav__items">
   <?php foreach ($rows as $row): ?>
     <span class="context-nav__item">

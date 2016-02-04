fc9c227c01b609d9c969b6b6c654822b883b276b
Harings Rob <haringsrob@gmail.com>
2016-02-04 10:13:19 +0100

    NEXTEUROPA-8173 updated the date metatag
    Former-commit-id: 075fb4ec47df9022ca227172b0fa26fb0dbc48e4
    Former-commit-id: dd1be11dfacd84b0b4e25b15321770955499220e

--- src/LanguageTokenHandler.php
+++ src/LanguageTokenHandler.php
@@ -14,7 +14,6 @@ namespace Drupal\dt_shared_functions;
  */
 class LanguageTokenHandler extends \Drupal\nexteuropa_token\TokenAbstractHandler {
 
-  const TOKEN_TYPE = 'dt_tokens';
   const CONTENT_LANGUAGE = 'dt_content_language';
   const INTERFACE_LANGUAGE = 'dt_interface_language';
 
@@ -26,11 +25,11 @@ class LanguageTokenHandler extends \Drupal\nexteuropa_token\TokenAbstractHandler
     $replacements = array();
 
     // Our tokens.
-    if ($type == self::TOKEN_TYPE) {
+    if ($type == _dt_shared_function_get_token_type()) {
       foreach ($tokens as $name => $original) {
         switch ($name) {
-          case self::CONTENT_LANGUAGE :
-            if($node = menu_get_object()) {
+          case self::CONTENT_LANGUAGE:
+            if ($node = menu_get_object()) {
               // If we can load our node, we will use that for our language.
               $language_token_value = _dt_shared_functions_content_language($node);
             }
@@ -42,7 +41,7 @@ class LanguageTokenHandler extends \Drupal\nexteuropa_token\TokenAbstractHandler
             $replacements[$original] = $language_token_value;
             break;
 
-          case self::INTERFACE_LANGUAGE :
+          case self::INTERFACE_LANGUAGE:
             $language_token_value = $language->language;
             // Set the replacement.
             $replacements[$original] = $language_token_value;
@@ -58,18 +57,13 @@ class LanguageTokenHandler extends \Drupal\nexteuropa_token\TokenAbstractHandler
    * {@inheritdoc}
    */
   public function hookTokenInfoAlter(&$data) {
-
-    // Define the group.
-    $data['types'][self::TOKEN_TYPE] = array(
-      'name' => t('Dt tokens'),
-      'description' => t('Dt custom tokens'),
-    );
+    $token = _dt_shared_function_get_token_type();
     // And the token.
-    $data['tokens'][self::TOKEN_TYPE][self::CONTENT_LANGUAGE] = array(
+    $data['tokens'][$token][self::CONTENT_LANGUAGE] = array(
       'name' => t("DT Content language"),
       'description' => t("The real language of the content - not the interface."),
     );
-    $data['tokens'][self::TOKEN_TYPE][self::INTERFACE_LANGUAGE] = array(
+    $data['tokens'][$token][self::INTERFACE_LANGUAGE] = array(
       'name' => t("DT Interface language"),
       'description' => t("The langauge of the interface."),
     );

f74f5905484d4c2ab4175154fc72c93aa4479f65
Harings Rob <haringsrob@gmail.com>
2016-04-08 15:41:27 +0200

    DTTSB-327 end of week commit for inpage nav

---
+++ templates/inpage_nav_block.tpl.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @file
+ * Theme file for inpage nav.
+ */
+?>
+<?php if ($title): ?>
+  <h3 class="block__title inpage-nav__block-title"><?php echo $title; ?></h3>
+<?php endif; ?>
+<div class="inpage-nav is-scrollspy-target">
+  <?php if ($links): ?>
+    <?php echo $links; ?>
+  <?php endif; ?>
+</div>

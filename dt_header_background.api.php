9a4ee5f160c73f7aa7930db94b669db5f5757987
Harings Rob <haringsrob@gmail.com>
2016-06-01 08:46:45 +0200

    DTTSB-504 Improvement for non full view mode content types

---
+++ dt_header_background.api.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @file
+ * Contains all hook defenitions.
+ */
+
+/**
+ * This hook can be used to define custom view modes.
+ *
+ * @return array
+ *   Array with elements:
+ *   'view_mode', the view mode to add the background for.
+ *   'content_types', to limit it to the content type, * for all content types.
+ */
+function hook_dt_header_background_info() {
+  return [
+    [
+      'view_mode' => 'full',
+      'content_type' => '*',
+    ],
+  ];
+}

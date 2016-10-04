b24c3d863a7a531897926f615d9355576efbcc6c
Harings Rob <haringsrob@gmail.com>
2016-10-04 14:26:22 +0200

    DTTSB-992 (Improved solution to alter the label) (#784)
    * Improved solution to alter the label
    
    * Fix behat symlink
    
    * Added test

--- plugins/selection/DtSharedFunctionsEntityReferenceSelectionHandler.class.php
+++ plugins/selection/DtSharedFunctionsEntityReferenceSelectionHandler.class.php
@@ -30,15 +30,56 @@ class DtSharedFunctionsEntityReferenceSelectionHandler extends EntityReference_S
   }
 
   /**
-   * Implements EntityReferenceHandler::getLabel().
+   * Implements EntityReferenceHandler::getReferencableEntities().
    */
-  public function getLabel($entity) {
+  public function getReferencableEntities($match = NULL, $match_operator = 'CONTAINS', $limit = 0) {
+    $options = [];
+    $entity_type = $this->field['settings']['target_type'];
+
+    $query = $this->buildEntityFieldQuery($match, $match_operator);
+    if ($limit > 0) {
+      $query->range(0, $limit);
+    }
+
+    $results = $query->execute();
+
+    if (!empty($results[$entity_type])) {
+      $entities = entity_load($entity_type, array_keys($results[$entity_type]));
+      foreach ($entities as $entity_id => $entity) {
+        list(, , $bundle) = entity_extract_ids($entity_type, $entity);
+        $options[$bundle][$entity_id] = check_plain($this->getFormLabel($entity));
+      }
+    }
+
+    return $options;
+  }
+
+  /**
+   * Generates the form label.
+   *
+   * @param object $entity
+   *   The entity to get the label for.
+   * @param bool $include_id
+   *   To include or not include the node id to the label.
+   *
+   * @return string
+   *   The label for the entity.
+   */
+  public function getFormLabel($entity, $include_id = TRUE) {
     // We use not the system name but readable name.
     $node_types = &drupal_static('dt_shared_functions_node_types');
     if (!isset($node_types)) {
       $node_types = _node_types_build()->names;
     }
-    $label = parent::getLabel($entity) . ' (' . (isset($node_types[$entity->type]) ? $node_types[$entity->type] : $entity->type) . '-' . $entity->nid . ')';
+
+    // Build the label.
+    $label = parent::getLabel($entity);
+    $label .= ' (' . (isset($node_types[$entity->type]) ? $node_types[$entity->type] : $entity->type);
+    if ($include_id) {
+      $label .= ' - ' . $entity->nid;
+    }
+    $label .= ')';
+
     return $label;
   }
 

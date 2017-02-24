9764178d2995935452fcf311c05e1c828c6de1d2
Nelson Matias <grin.correio@gmail.com>
2017-02-24 03:31:34 +0100

    Removed hitme templates and applied the hitme logic to the templates.

--- nexteuropa_ds_layouts/layouts/ne_bootstrap_3_9_stacked_conditional/ne-bootstrap-3-9-stacked_conditional.tpl.php
+++ nexteuropa_ds_layouts/layouts/ne_bootstrap_3_9_stacked_conditional/ne-bootstrap-3-9-stacked_conditional.tpl.php
@@ -5,18 +5,21 @@
  * Display Suite NE Bootstrap Three Columns Stacked.
  */
 
-  // Add sidebar classes so that we can apply the correct width in css.
-  // Second block is needed to activate display suite support on forms.
+// Add sidebar classes so that we can apply the correct width in css.
+// Second block is needed to activate display suite support on forms.
 ?>
 
-<<?php print $layout_wrapper; print $layout_attributes; ?> class="<?php print $classes; ?>">
-  <?php if (isset($title_suffix['contextual_links'])): ?>
-    <?php print render($title_suffix['contextual_links']); ?>
-  <?php endif; ?>
+<<?php print $layout_wrapper . $layout_attributes; ?> class="<?php print $classes; ?>">
+
+<?php if (isset($title_suffix['contextual_links'])): ?>
+  <?php print render($title_suffix['contextual_links']); ?>
+<?php endif; ?>
+
   <div class="row">
     <<?php print $left_header_wrapper; ?> class="col-lg-9 <?php print $left_header_classes; ?>">
       <?php print $left_header; ?>
     </<?php print $left_header_wrapper; ?>>
+
     <<?php print $right_header_wrapper; ?> class="col-lg-3 <?php print $right_header_classes; ?>">
       <?php print $right_header; ?>
     </<?php print $right_header_wrapper; ?>>
@@ -26,13 +29,13 @@
       <?php print $top; ?>
     </<?php print $top_wrapper; ?>>
 
-    <?php if (isset($left) && !empty($left)): ?>
+    <?php if (!empty($left)): ?>
       <<?php print $left_wrapper; ?> class="col-lg-3 <?php print $left_classes; ?>">
         <?php print $left; ?>
       </<?php print $left_wrapper; ?>>
     <?php endif; ?>
 
-    <?php if (isset($left) && !empty($left)): ?>
+    <?php if (!empty($left)): ?>
       <<?php print $central_wrapper; ?> class="col-lg-9 <?php print $central_classes; ?>">
     <?php else: ?>
       <<?php print $central_wrapper; ?> class="col-lg-12 <?php print $central_classes; ?>">
@@ -40,8 +43,9 @@
       <?php print $central; ?>
     </<?php print $central_wrapper; ?>>
   </div>
-</<?php print $layout_wrapper ?>>
+
+</<?php print $layout_wrapper; ?>>
 
 <?php if (!empty($drupal_render_children)): ?>
-  <?php print $drupal_render_children ?>
+  <?php print $drupal_render_children; ?>
 <?php endif; ?>
--- nexteuropa_ds_layouts/layouts/ne_featured_item/ne-featured-item.tpl.php
+++ nexteuropa_ds_layouts/layouts/ne_featured_item/ne-featured-item.tpl.php
@@ -7,32 +7,35 @@
 ?>
 
 <div <?php print $layout_attributes; ?> class="<?php print $classes; ?>">
-<?php if (isset($title_suffix['contextual_links'])) : ?>
+
+<?php if (isset($title_suffix['contextual_links'])): ?>
   <?php print render($title_suffix['contextual_links']); ?>
 <?php endif; ?>
-<?php if (!isset($hidelink) || (isset($hidelink) && $hidelink == FALSE)) : ?>
-  <a href="<?php print $node_url; ?>" class="listing__item-link">
+
+<?php if (!isset($prevent_link)): ?>
+  <div class="listing__item-link">
+<?php else: ?>
+  <div class="listing__item-nolink">
 <?php endif; ?>
 
-  <div class="featured-item">
-    <?php if (!empty($featured_image)) : ?>
-    <div class="featured-item__image">
-      <?php print $featured_image; ?>
+    <div class="featured-item">
+      <?php if (!empty($featured_image)): ?>
+        <div class="featured-item__image">
+          <?php print $featured_image; ?>
+        </div>
+      <?php endif; ?>
+      <section class="featured-item__content">
+        <div class="meta meta--header">
+          <?php print $meta; ?>
+        </div>
+        <?php print $main; ?>
+      </section>
     </div>
-    <?php endif; ?>
-    <section class="featured-item__content">
-      <div class="meta meta--header">
-        <?php print $meta;  ?>
-      </div>
-      <?php print $main; ?>
-    </section>
+
   </div>
 
-<?php if (!isset($hidelink) || (isset($hidelink) && $hidelink == FALSE)) : ?>
-  </a>
-<?php endif; ?>
 </div>
 
-<?php if (!empty($drupal_render_children)) : ?>
+<?php if (!empty($drupal_render_children)): ?>
   <?php print $drupal_render_children; ?>
 <?php endif; ?>

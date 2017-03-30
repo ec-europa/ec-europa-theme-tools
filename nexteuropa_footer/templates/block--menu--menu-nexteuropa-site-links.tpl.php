8aa14e2b8bde2cb15ffa96920e2b64c1954490a5
Nelson Matias <grin.correio@gmail.com>
2017-03-31 01:05:49 +0200

    Minor improvement.

--- nexteuropa_footer/templates/block--menu--menu-nexteuropa-site-links.tpl.php
+++ nexteuropa_footer/templates/block--menu--menu-nexteuropa-site-links.tpl.php
@@ -46,13 +46,9 @@
  * @ingroup themeable
  */
 ?>
-
 <?php print render($title_prefix); ?>
-<?php if (!empty($title)) : ?>
+<?php if (!empty($title)): ?>
   <h4 class="footer__title" <?php print $title_attributes; ?>><?php print $title; ?></h4>
-<?php endif;?>
+<?php endif; ?>
 <?php print render($title_suffix); ?>
-
 <?php print $content; ?>
-
-

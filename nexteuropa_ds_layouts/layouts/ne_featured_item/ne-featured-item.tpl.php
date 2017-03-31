b1c9688f0027a8ced19479e2a0e9d27e1d5b9f55
pedroraf <pedrorcf@gmail.com>
2017-03-31 11:11:11 +0200

    DTTSB-1919: improvement to tpl, check if meta not empty

--- nexteuropa_ds_layouts/layouts/ne_featured_item/ne-featured-item.tpl.php
+++ nexteuropa_ds_layouts/layouts/ne_featured_item/ne-featured-item.tpl.php
@@ -7,28 +7,30 @@
 ?>
 
 <div <?php print $layout_attributes; ?> class="<?php print $classes; ?>">
-<?php if (isset($title_suffix['contextual_links'])) : ?>
-  <?php print render($title_suffix['contextual_links']); ?>
-<?php endif; ?>
-<?php if (!isset($hidelink) || (isset($hidelink) && $hidelink == FALSE)) : ?>
+  <?php if (isset($title_suffix['contextual_links'])) : ?>
+    <?php print render($title_suffix['contextual_links']); ?>
+  <?php endif; ?>
+  <?php if (!isset($hidelink) || (isset($hidelink) && $hidelink == FALSE)) : ?>
   <a href="<?php print $node_url; ?>" class="listing__item-link">
-<?php endif; ?>
-
-  <div class="featured-item">
-    <?php if (!empty($featured_image)) : ?>
-    <div class="featured-item__image">
-      <?php print $featured_image; ?>
-    </div>
     <?php endif; ?>
-    <section class="featured-item__content">
-      <div class="meta meta--header">
-        <?php print $meta;  ?>
-      </div>
-      <?php print $main; ?>
-    </section>
-  </div>
 
-<?php if (!isset($hidelink) || (isset($hidelink) && $hidelink == FALSE)) : ?>
+    <div class="featured-item">
+      <?php if (!empty($featured_image)) : ?>
+        <div class="featured-item__image">
+          <?php print $featured_image; ?>
+        </div>
+      <?php endif; ?>
+      <section class="featured-item__content">
+        <?php if (!empty($meta)) { ?>
+          <div class="meta meta--header">
+            <?php print $meta; ?>
+          </div>
+        <?php } ?>
+        <?php print $main; ?>
+      </section>
+    </div>
+
+    <?php if (!isset($hidelink) || (isset($hidelink) && $hidelink == FALSE)) : ?>
   </a>
 <?php endif; ?>
 </div>
